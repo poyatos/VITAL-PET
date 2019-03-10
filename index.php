5ce46fc17bf2c792a0d973a638bea38539f8035f
pablo <p.poyatos@hotmail.com>
2019-03-10 18:39:39 +0100

    Cambio de diseño

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 </head>
 <body>
 
+
+<!-- MENÚ PRINCIPAL -->
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -23,7 +25,7 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a></li>
+        <li ><a class="in" href="#"><span class="glyphicon glyphicon-log-in in"></span> Iniciar sesión</a></li>
       </ul>
     </div>
   </div>
@@ -37,12 +39,12 @@
     </ol>    
     <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="IMAGENES/gat.jpg" alt="Image">
+        <img src="IMAGENES/baner.png" alt="Image">
         <div class="carousel-caption">
         </div>      
       </div>
       <div class="item">
-        <img src="IMAGENES/lag.jpg" alt="Image">
+        <img src="IMAGENES/baner1.png" alt="Image">
         <div class="carousel-caption">
         </div>      
       </div>
@@ -60,38 +62,96 @@
 
 
  <!-- CONTENIDO --> 
-<div class="container text-center">    
-  <h3>Tu clinica de confianza</h3><br>
+
+<div class="container-fluid ">
   <div class="row">
-    <div class="col-sm-6">
-      <img src="IMAGENES/vet.jpg" class="img-responsive" style="width:100%" alt="Image">
-      <p>Animales</p>
+    <div class="col-sm-8">
+      <h2>SOBRE NOSOTROS</h2>
+      <h4>VITAL PET</h4>      
+      <p>TU VETERINARIO DE CONFIANZA</p>
+      <p>VITAL PET es una empresa nacida en 1930 en la lucha opresora por liberar a los animales de sus jaulas
+        corporeas y precupandose por el medioambiente, inaugurado por viniloko en 1950 de manera oficial
+        esta empresa lleva en auge mas de 50 años dandolo todo en las pistas de baile, el resto del texto es inventado
+        como prueba para ver que tal queda maquetado es una empresa nacida en 1930 en la lucha opresora por liberar a los animales de sus jaulas
+        corporeas y precupandose por el medioambiente, inaugurado por viniloko en 1950 de manera oficial
+        esta empresa lleva en auge mas de 50 años dandolo todo en las pistas de baile, el resto del texto es inventado
+        como prueba para ver que tal queda maquetado es una empresa nacida en 1930 en la lucha opresora por liberar a los animales de sus jaulas
+        corporeas y precupandose por el medioambiente, inaugurado por viniloko en 1950 de manera oficial
+        esta empresa lleva en auge mas de 50 años dandolo todo en las pistas de baile, el resto del texto es inventado
+        como prueba para ver que tal queda maquetado
+      </p>
     </div>
-    <div class="col-sm-6"> 
-      <img src="IMAGENES/vet.jpg" class="img-responsive" style="width:100%" alt="Image">
-      <p>Personas</p>    
-    </div>
-
-    <div class="col-sm-6">
-    <p>Este texto es de ejemplo para probar el index, jajaja xd miguel chino carlos calvo y pablo cabezon
-     Este texto es de ejemplo para probar el index, jajaja xd miguel chino carlos calvo y pablo cabezon</p>
-    </div>
-
-    <div class="col-sm-6">
-    <p>Este texto es de ejemplo para probar el index, jajaja xd miguel chino carlos calvo y pablo cabezon
-     Este texto es de ejemplo para probar el index, jajaja xd miguel chino carlos calvo y pablo cabezon</p>
+    <div class="col-sm-4">
+    <img class="logotipo" src="IMAGENES/sobre.png" width="500px" height="auto" >
     </div>
   </div>
-</div><br>
+</div>
+
+<div class="container-fluid bg-grey">
+  <div class="row">
+    <div class="col-sm-4">
+    <img class="logotipo" src="IMAGENES/valors.png" width="500px" height="auto" >
+    </div>
+    <div class="col-sm-8">
+      <h2>NUESTROS VALORES</h2>
+      <h4><strong>MISIÓN DE LA EMPRESA</strong> Una mascota no es solo un animal es un compañero y amigo que te acompaña durante tu vida, por eso desde PET VITAL ofrecemos el servicio numero 1 para que tu mascota viva sana y en las mejores condiciones posibles</h4>      
+      
+    </div>
+  </div>
+</div>
+
+<!-- SERVICIOS -->
+<div class="container-fluid text-center ">
+  <h2>SERVICIOS</h2>
+  <h4>QUE PODEMOS OFRECERTE</h4>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-eye-open logo-small"></span>
+      <h4>REVISIONES</h4>
+      <p>Revisiones periodicas para revisar el estado de tu mascota</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-gift logo-small"></span>
+      <h4>SERVICIOS DE BELLEZA</h4>
+      <p>Tu mejor amigo se merece los mejores servicios de belleza</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-certificate logo-small"></span>
+      <h4>ACCIDENTES</h4>
+      <p>Cualquier tipo de accidente u operación</p>
+    </div>
+  </div>
+  <br><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-warning-sign logo-small"></span>
+      <h4>SERVICIO DE URGENCIA</h4>
+      <p>Servicios 24 horas para tu compañero</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-scissors logo-small"></span>
+      <h4>CASTRACIÓN</h4>
+      <p>Los mejores metodos de castración para tu animal</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-tint logo-small"></span>
+      <h4 style="color:#303030;">VACUNAS</h4>
+      <p>Vacunas periódicas para tu mascota</p>
+    </div>
+  </div>
+</div>
+
+
 
 
 <div class="container">
     <div class="row-fluid">
-        <div class="col-md-8">
+        <div class="col-md-9">
         	<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/place?q=Cyberborg%20Bodyart%2C%20Bangka%2C%20Jakarta%2C%20Indonesia&key=AIzaSyCGz8WzqxQw1OwHWey3LCTjqKFG9feCxP4"></iframe>
     	</div>
     	
-      	<div class="col-md-4">
+      	<div class="col-md-3">
     		<h2>CONTACTO</h2>
     		<address>
     			<strong>VITAL PET</strong><br>
@@ -108,7 +168,7 @@
 
 <br/>
 <footer class="container-fluid text-center">
-  <p>copiright de viniloko y charlie</p>
+  <p>© Carlos J Angulo, Miguel Gallego, Pablo Fernández</p>
 </footer>
 
 </body>
