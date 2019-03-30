05b64309f90678869fa120fd45df85c3e5b5fa0e
pablo <p.poyatos@hotmail.com>
2019-03-30 13:41:31 +0100

    controladores y paginación

--- CONTROLADOR/controladorCliente.php
+++ CONTROLADOR/controladorCliente.php
@@ -4,3 +4,12 @@ require_once "../BBDD/model.php";
 require_once "../BBDD/config.php";
 
 session_start();
+
+if(!isset($_SESSION['usuario']) && !isset($_SESSION['rol'])){
+    header("Location: ../index.php");
+    } else {
+        if($_SESSION['rol'] != 'Cliente'){
+            header("Location: ../VISTA/".$_SESSION['rol']);
+        }
+}
+
