30f2cad62bd9c870729cd70c6c95734afbc74a6b
pablo <p.poyatos@hotmail.com>
2019-04-20 15:49:05 +0200

    Validación de NIF/NIE

--- JS/validacion.js
+++ JS/validacion.js
@@ -6,9 +6,8 @@ boton.onclick = function() { Ç
 //validacion DNI
 var dni = document.getElementById("dni")
 var numero
-var le
 var letra
-var expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
+var expresion_regular_dni = /^[XYZ]?\d{5,8}[A-Z]$/;
 //validacion DNI
 
 //validacion PASS
@@ -16,22 +15,26 @@ var contrasena = document.getElementById("pwd")
 //validacion PASS
 
 //PABLO:  TEST -> Devuelve un valor booleano que indica si existe un patrón en una cadena buscada. (ER) nombre dada a la variable (expresión regular)
-if(expresion_regular_dni.test (er) == dni){ //condicional que compara un dni correcto (expresion regular) con el introducido
-   numero = er.substr(0,er.length-1);
-   le = er.substr(er.length-1,1);
+if(expresion_regular_dni.test(dni) === true){
+   numero = dni.substr(0,dni.length-1);
+   numero = numero.replace('X', 0);
+   numero = numero.replace('Y', 1);
+   numero = numero.replace('Z', 2);
+   let = dni.substr(dni.length-1, 1);
    numero = numero % 23;
-   letra='TRWAGMYFPDXBNJZSQVHLCKET'; //letras validas para dni
-   letra=letra.substring(numero,numero+1);
-
-  if (letra!=le.toUpperCase()) {
-     alert('La letra del Dni no se corresponde');
+   letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
+   letra = letra.substring(numero, numero+1);
+   if (letra != let) {
+       //alert('Dni erroneo, la letra del NIF no se corresponde');
+       return false;
    }else{
-     alert('Dni correcto'); // aqui esta correcto
+       //alert('Dni correcto');
+       return true;
    }
 }else{
-   alert('Dni erroneo, formato no válido');
- }
-
+   //alert('Dni erroneo, formato no válido');
+   return false;
+}
  //campos vacios
  if (contrasena.length == 0 || dni.length == 0 ) { 
     alert('Los campos no pueden estar vacios');
