f090993b8b17c44e8eaa3e998750be728d9406b3
Hipestw <carlosjah89@gmail.com>
2019-05-19 16:11:06 +0200

    Correción ortografía FPDF

--- FACTURAS/controladorPDF.php
+++ FACTURAS/controladorPDF.php
@@ -1,5 +1,6 @@
 <?php
             $pdf = new FPDF();
+            //$pdf->Text(20,30,utf8_decode($cad1));
 
             //pdf la factura numero 1 de la tipica BBDD de facturacion
             $pdf->AddPage();
@@ -11,7 +12,7 @@
             $pdf->SetFont('Arial', 'B', 14);
             $pdf->Cell(190, 10, "FACTURA DE CITA", 0, 2, "C");
             $pdf->SetFont('Arial', 'B', 10);
-            $pdf->MultiCell(190, 5, "Número de factura: ".$datos[0]['id_pago']."\n"."Fecha: ".date("d/m/Y", strtotime($datos[0]['fecha_pago'])), 0, "C", false);
+            $pdf->MultiCell(190, 5, utf8_decode("Número de factura: ").$datos[0]['id_pago']."\n"."Fecha: ".date("d/m/Y", strtotime($datos[0]['fecha_pago'])), 0, "C", false);
             $pdf->Ln(2);
 
             // Datos del veterinario
@@ -23,13 +24,13 @@
             $pdf->MultiCell(
                 190,
                 5,
-                "Vital-Pet \n".
+                utf8_decode("Vital-Pet \n".
             "Dirección: Calle de Móstoles 70A \n".
             "Población: Fuenlabrada \n".
             "Provincia: Madrid \n".
             "Código Postal: 28942 \n".
             "Teléfono: 910256254 / 605963254 \n".
-            "Fax: 910256254 / 605963254",
+            "Fax: 910256254 / 605963254"),
                 0,
                 "J",
                 false
@@ -43,13 +44,13 @@
             $pdf->MultiCell(
                 190,
                 5,
-                "Nombre: ".$datos[0]['nombre_usuario']."\n".
+                utf8_decode("Nombre: ".$datos[0]['nombre_usuario']."\n".
             "Apellidos: ".$datos[0]['apellidos_usuario']."\n".
             "Dirección: ".$datos[0]['direccion_usuario']."\n".
             "NIE / NIF: ".$datos[0]['dni_usuario']."\n".
             "Nombre de mascota: ".$datos[0]['nombre_mascota']."\n".
             "Tipo de mascota: ".$datos[0]['tipo_mascota']."\n".
-            "Raza de mascota: ".$datos[0]['raza_mascota']."\n",
+            "Raza de mascota: ".$datos[0]['raza_mascota']."\n"),
                 0,
                 "J",
                 false
@@ -66,8 +67,8 @@
 
                 foreach ($datos as $prueba) {
                     $pdf->SetFont('Arial', '', 10);
-                    $pdf->Cell(100, 6, $prueba["nombre_tipo_prueba"], 1, 0, 'C');
-                    $pdf->Cell(20, 6, $prueba["precio__tipo_prueba"] . chr(128), 1, 0, 'C');
+                    $pdf->Cell(100, 6, utf8_decode($prueba["nombre_tipo_prueba"]), 1, 0, 'C');
+                    $pdf->Cell(20, 6, utf8_decode($prueba["precio__tipo_prueba"]) . chr(128), 1, 0, 'C');
                     $pdf->Ln(8);
                 }
 
